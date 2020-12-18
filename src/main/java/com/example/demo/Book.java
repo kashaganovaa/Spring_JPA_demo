9ebdbde922d7bd0fe710935878b135a7513b3c25
kashaganovaa <kashaganova1m@gmail.com>
2020-12-18 23:02:07 +0600

    Initial commit

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Book() {
+    }
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
